d3cee8e9ff12a88cbd2b8955de5cbaf7654771c4
efak77 <147770610+efak77@users.noreply.github.com>
2024-10-08 09:26:48 -0300

    Create trabalho.py

---
+++ trabalho.py
@@ -0,0 +1,22 @@
+print("faça seu cadastro da pessoa  aqui abaixo:")
+sexo1=input("insira seu sexo:")
+idade1=int(input("coloque sua idade:"))
+nome1=input("coloque seu nome:")
+print("\n")
+
+print("cadastramento concluído com sucesso")
+print("\n")
+
+print("faça seu agendamento aqui abaixo:")
+horário1=int(input("coloque seu horário:"))
+nome1=input("coloque seu nome:")
+
+
+print("\n")
+print("cadastro1:", sexo1,idade1,nome1,  sep='/')
+print("\n")
+print("cadastramento concluído com sucesso")
+print("\n")
+print( nome1, horário1,    sep='/')
+print("\n")
+print("agendamento concluído com sucesso")
